43489d33656c32fc8c386ed9da7e42baf929b58f
DariaRev <dr.ea.r@ya.ru>
2021-05-11 17:12:37 +0300

    sixth_task

---
+++ route.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative 'station'
+require_relative 'instance_counter'
+
+class Route
+  include InstanceCounter
+  include Validation
+  attr_reader :other_stations, :first_station, :last_station
+
+  validate :first_station, :type, Station
+  validate :last_station, :type, Station
+  def initialize(first_station, last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @other_stations = []
+    validate!
+    register
+  end
+
+  def add_station(station)
+    @other_stations << station
+  end
+
+  def remove_station(station)
+    @other_stations.delete(station)
+  end
+
+  def get_stations
+    [first_station, @other_stations, last_station].flatten
+  end
+
+  def info
+    sts = []
+    get_stations.each { |station| sts << station.name }
+    "Описание маршрута: #{sts.join(' - ')}"
+  end
+end
---
+++ station.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative 'instance_counter'
+require_relative 'train'
+class Station
+  include InstanceCounter
+  include Validation
+
+  attr_reader :trains_now, :name
+
+  validate :name, :presence
+
+  def initialize(name)
+    @name = name.to_s
+    @trains_now = []
+    validate!
+    register
+  end
+
+  def trains_each(&block)
+    trains_now.each do |train|
+      block.call(train)
+    end
+  end
+
+  def self.all
+    ObjectSpace.each_object(Station).to_a
+  end
+
+  def add_train(train)
+    @trains_now << train
+  end
+
+  def send_train(train)
+    trains_now.delete(train)
+  end
+
+  def trains_by(type)
+    trains_now.select { |train| train.type == type }
+  end
+
+  def count_trains_by(type)
+    trains_by(type).length
+  end
+end
